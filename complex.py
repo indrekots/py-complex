fea81563cba65624ad2d41c815051758f179360b
Indrek Ots <ots.indrek@gmail.com>
2015-09-16 19:03:57 +0300

    add tests, implement to string

--- complex.py
+++ complex.py
@@ -7,4 +7,15 @@ class Complex:
         self.imaginary = float(i)
 
     def __str__(self):
-        return "%.2f %s %.2fi" % (self.real, "+" if self.imaginary > 0 else "-", abs(self.imaginary))
+        r_str = "%.2f" % self.real if self.real != 0 else ""
+        i_str = "%.2fi" % abs(self.imaginary) if self.imaginary != 0 else ""
+        return "{}{}{}".format(r_str, self.__sign(self.imaginary) if self.imaginary else "", i_str)
+
+    def __sign(self, n):
+        if self.real == 0:
+            if n < 0: return "-"
+            else: return ""
+        elif self.real != 0:
+            if n < 0: return " - "
+            else: return " + "
+        else: return ""
--- main.py
+++ main.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     print(Complex(1, -2))
     print(Complex(0, -2))
     print(Complex(-1, -2))
+    print(Complex(0, -999))
